48575f5c3362982f5078a3942fadbef59666ee07
mjuniper <mjuniper@gmail.com>
2020-05-14 13:50:30 -0600

    use nested routes and rendertemplate

--- app/router.js
+++ app/router.js
@@ -9,12 +9,14 @@ export default class Router extends EmberRouter {
 Router.map(function() {
   this.route('datasets', function() {
     this.route('dataset', { path: '/:dataset_id' }, function () {
-      // this.mount('edit-engine', { path: '/edit' }); // if we do it this way we don't get params, we do get the resolved model but we are in the outlet of datasets.dataset
+      this.mount('edit-engine', { path: '/edit' }); // if we do it this way we don't get params, we do get the resolved model but we are in the outlet of datasets.dataset
     });
-    this.mount('edit-engine', { path: '/:dataset_id/edit' }); // if we do it this way we get params.dataset_id, but we do not get the resolved model, and we are not in our datasets.dataset outle
+    // this.mount('edit-engine', { path: '/:dataset_id/edit' }); // if we do it this way we get params.dataset_id, but we do not get the resolved model, and we are not in our datasets.dataset outlet
   });
   this.route('documents', function() {
-    this.route('document', { path: '/:document_id' });
-    this.mount('edit-engine', { path: '/:document_id/edit' });
+    this.route('document', { path: '/:document_id' }, function () {
+      this.mount('edit-engine', { path: '/edit' });
+    });
+    // this.mount('edit-engine', { path: '/:document_id/edit' });
   });
 });
